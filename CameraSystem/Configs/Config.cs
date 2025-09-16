23fbeecdacb37129dfbc6185dfabd8ddd3554a66
gamendegamer321 <gamendegamer321@gmail.com>
2025-09-16 19:59:57 +0200

    Updated because of langversion difference

--- CameraSystem/Configs/Config.cs
+++ CameraSystem/Configs/Config.cs
@@ -14,12 +14,12 @@ public class Config
     [Description("Preset locations where workstations will be automatically spawned")]
     public PresetConfig[] PresetConfigs { get; set; } =
     {
-        new(RoomName.HczArmory, new(1.1f, 0f, 2.1f), new(0f, 180f, 0f), Vector3.one),
-        new(RoomName.EzIntercom, new(-5.4f, 0f, -1.8f), Vector3.zero, Vector3.one),
-        new(RoomName.EzIntercom, new(-6.9f, -5.8f, 1.2f), new(0f, 90f, 0f), new(1f, 1f, 0.7f)),
-        new(RoomName.HczWarhead, new(2f, -72.4f, 8.5f), Vector3.zero, Vector3.one),
-        new(RoomName.Lcz914, new(-1.9f, 0f, 5.5f), new(0f, 90f, 0f), Vector3.one),
-        new(RoomName.Lcz914, new(-6.2f, 0f, 3.1f), new(0f, 180f, 0f), Vector3.one)
+        new(RoomName.HczArmory, new Vector3(1.1f, 0f, 2.1f), new Vector3(0f, 180f, 0f), Vector3.one),
+        new(RoomName.EzIntercom, new Vector3(-5.4f, 0f, -1.8f), Vector3.zero, Vector3.one),
+        new(RoomName.EzIntercom, new Vector3(-6.9f, -5.8f, 1.2f), new Vector3(0f, 90f, 0f), new Vector3(1f, 1f, 0.7f)),
+        new(RoomName.HczWarhead, new Vector3(2f, -72.4f, 8.5f), Vector3.zero, Vector3.one),
+        new(RoomName.Lcz914, new Vector3(-1.9f, 0f, 5.5f), new Vector3(0f, 90f, 0f), Vector3.one),
+        new(RoomName.Lcz914, new Vector3(-6.2f, 0f, 3.1f), new Vector3(0f, 180f, 0f), Vector3.one)
     };
 
     [Description("List of camera workstation configurations including their positions, rotations, and scales")]
@@ -30,7 +30,7 @@ public class Config
     };
 
     [Description("Roles that are prohibited from interacting with camera workstations")]
-    public RoleTypeId[] ProhibitedRoles { get; set; } = [];
+    public RoleTypeId[] ProhibitedRoles { get; set; } = { };
 
     [Description("The translations used")] public Translation Translations { get; set; } = new();
 }
